a174869e12ace67e8d8d622b70bc4cc218c2281a
Alberto Molpeceres <alberto.molpeceres@gmail.com>
2009-02-18 21:24:44 +0100

    reactivated helper in init.rb and removed unnecessary requiered plugins, they are already required in tog_core

--- init.rb
+++ init.rb
@@ -1,8 +1,6 @@
 require_plugin 'tog_core'
 require_plugin 'seo_urls'
 require_plugin 'attachment_fu'
-require_plugin 'acts_as_rateable'
-require_plugin 'acts_as_taggable_on_steroids'
 
 Tog::Interface.sections(:site).add "Files", "/depot/files"     
 
@@ -11,4 +9,4 @@ Tog::Interface.sections(:member).add "My files", "/member/depot/files"
 Tog::Plugins.settings :tog_depot, "file.max_size_file"	=> "5000",
                                   "file.max_size_storage"	=> "500000"
 
-#Tog::Plugins.helpers Files::FilesHelper
+Tog::Plugins.helpers Depot::FilesHelper
